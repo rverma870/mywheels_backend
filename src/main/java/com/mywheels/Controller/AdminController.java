e2c723586276711420937986386ebbf0cc517d1a
rverma870 <rverma870@gmail.com>
2022-02-20 15:43:48 +0530

    deleted unused comments and file

--- src/main/java/com/mywheels/Controller/AdminController.java
+++ src/main/java/com/mywheels/Controller/AdminController.java
@@ -2,14 +2,11 @@ package com.mywheels.Controller;
 
 import java.util.List;
 
-import com.mywheels.Helper.Test;
 import com.mywheels.Model.Product;
-import com.mywheels.Model.User;
 import com.mywheels.Service.ProductService;
 import com.mywheels.Service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,22 +21,9 @@ public class AdminController {
     @Autowired
     UserService userService;
 
-    @Autowired
-    Test test;
-
     @GetMapping("/admin/all" )
     public List<Product> getAlProducts(){
         return productService.getAllProduct();
     }
 
-    @GetMapping("admin/user/getAll")
-    public ResponseEntity<List<User>> getAllUser(){
-        return ResponseEntity.ok().body(userService.getAllUsers());
-    }
-    
-
-    @GetMapping("/product/test")
-    public void getsdfAllUser(){
-      System.out.println(test.getAccesskey() + " this is access key");
-    }
 }
